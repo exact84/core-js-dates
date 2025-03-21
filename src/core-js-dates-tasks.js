2a76f0bfc0e896998a7a6830d616a9e6cb408ac5
Alexey Trukhlyayev <exact84@gmail.com>
2025-03-22 00:54:49 +0500

    feat: add 2 more tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -100,8 +100,10 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const date1 = new Date(dateStart);
+  const date2 = new Date(dateEnd);
+  return (date2 - date1) / (1000 * 60 * 60 * 24) + 1;
 }
 
 /**
@@ -121,8 +123,11 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const date1 = new Date(date);
+  const date2 = new Date(period.start);
+  const date3 = new Date(period.end);
+  return date1 >= date2 && date1 <= date3;
 }
 
 /**
